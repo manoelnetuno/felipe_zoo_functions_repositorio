a3a7d5fc914f8ef43a9825050b0a8e889e38e0a3
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-11-02 17:58:27 -0300

    requisito count animal

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 const countAnimals = (animal) => {
-  // seu código aqui
+  let result = {};
+  species.forEach((specie) => { result[specie.name] = specie.residents.length; });
+  if (animal) {
+    const specieActual = animal.species;
+    const foundAnimal = species.find((specie) => specie.name === specieActual);
+    result = foundAnimal.residents.length;
+    if (animal.sex) {
+      result = foundAnimal.residents.filter((resident) => resident.sex === animal.sex).length;
+    }
+  }
+  return result;
 };
 
 module.exports = countAnimals;
