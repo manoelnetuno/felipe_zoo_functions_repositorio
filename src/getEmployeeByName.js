85c9027a92e65cfde3b85cfac9134784cb997e15
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-11-02 17:11:04 -0300

    requisito 3,4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  const employee = data.employees.find(
+    (emp) => emp.firstName === employeeName || emp.lastName === employeeName,
+  );
+
+  return employee || {};
 };
 
 module.exports = getEmployeeByName;
