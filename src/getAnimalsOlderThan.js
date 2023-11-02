ea2999057312a7b73f9ff0672e51c9a4f7b0af98
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-11-02 14:47:29 -0300

    requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const checkedAnimal = data.species.find((specie) => specie.name === animal);
+  const residentAncients = checkedAnimal.residents.filter((resident) => resident.age >= age);
+  const filterAgeAnimal = residentAncients.length === checkedAnimal.residents.length;
+  return filterAgeAnimal;
 };
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  !!!!
+const getSpeciesByIds = (...ids) => {
+  const arraySpeciesByIds = [];
+  if (!ids) return arraySpeciesByIds;
+
+  ids.forEach((id) => {
+    const filter = data.species.find((idAnimal) => idAnimal.id === id);
+    if (filter) {
+      arraySpeciesByIds.push(filter);
+    }
+  });
+
+  return arraySpeciesByIds;
 };
 
 module.exports = getSpeciesByIds;
