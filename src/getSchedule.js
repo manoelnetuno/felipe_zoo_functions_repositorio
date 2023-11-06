844367fe55760809f221aadf4cb40e28a524e25f
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-11-05 21:25:28 -0300

    requisito get schedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
+const closedDayMessage = (day) => ({
+  [day]: {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  },
+});
+
+function getScheduleDay(day) {
+  const closedDay = 'Monday';
+  const { open, close } = data.hours[day];
+
+  if (day === closedDay) return closedDayMessage(day);
+
+  return {
+    [day]: {
+      officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition: data.species.filter((animal) => animal.availability.includes(day))
+        .map(({ name }) => name),
+    },
+  };
+}
+
+function getAllSchedule() {
+  const allDays = Object.keys(data.hours);
+
+  return allDays.reduce((days, day) => {
+    const theDay = getScheduleDay(day);
+    return {
+      ...days,
+      ...theDay,
+    };
+  }, {});
+}
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const getAnimalDays = data.species.find((animal) => animal.name === scheduleTarget);
+  if (getAnimalDays) return getAnimalDays.availability;
+  if (scheduleTarget in data.hours) return getScheduleDay(scheduleTarget);
+  return getAllSchedule();
 };
 
 module.exports = getSchedule;
