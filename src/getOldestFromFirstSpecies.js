e5f8f2deff1499ba9f7b9cafd689171a697673fe
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-11-05 23:06:23 -0300

    requisito oldestfromfristspecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employee = data.employees.find((element) => element.id === id);
+  const firstSpeciesId = employee.responsibleFor[0];
+  const species = data.species.find((element) => element.id === firstSpeciesId);
+  const oldestAnimal = species.residents.reduce((a, b) => (a.age > b.age ? a : b));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
