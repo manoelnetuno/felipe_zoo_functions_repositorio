8284dc7aea9275b4c1750e3908b726c4ea585cea
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2023-11-06 20:27:53 -0300

    requisito employecover

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
+const getEmployeeInfo = (employeeInfo) => {
+  const theEmployee = data.employees.find((employee) => employee.firstName === employeeInfo.name
+  || employee.lastName === employeeInfo.name
+  || employee.id === employeeInfo.id);
+
+  if (typeof theEmployee === 'undefined') throw new Error('Informações inválidas');
+
+  return theEmployee;
+};
+
+const getSpeciesName = (id) => data.species.find((specie) => specie.id === id).name;
+const getSpeciesLocation = (id) => data.species.find((specie) => specie.id === id).location;
+
+function getEmployee(employee) {
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor.map((idAnimal) => getSpeciesName(idAnimal)),
+    locations: employee.responsibleFor.map((idAnimal) => getSpeciesLocation(idAnimal)),
+  };
+}
+
+function getAllEmployees() {
+  const employees = data.employees.reduce((allEmployees, employee) => {
+    const formattedEmployee = getEmployee(employee);
+    return [
+      ...allEmployees,
+      formattedEmployee,
+    ];
+  }, []);
+  return employees;
+}
+
+const getEmployeesCoverage = (employeeInfo) => {
   // seu código aqui
+  try {
+    if (!employeeInfo) return getAllEmployees();
+    const theEmployee = getEmployeeInfo(employeeInfo);
+    return getEmployee(theEmployee);
+  } catch (error) {
+    throw error.message;
+  }
 };
 
 module.exports = getEmployeesCoverage;
